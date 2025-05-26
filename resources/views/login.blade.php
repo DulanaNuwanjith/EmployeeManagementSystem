12e548dd5e69d6d635359a72a89bc2fa77483c0d
DulanaNuwanjith99 <dulana@hsenidlanka.com>
2025-05-26 12:22:56 +0530

    bug fix icon

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -45,10 +45,8 @@
                 class="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-600 hover:text-amber-600"
                 aria-label="Toggle Password Visibility">
           <svg id="eyeIcon" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mt-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477 0-8.268-2.943-9.542-7a9.957 9.957 0 012.647-4.435m1.75-1.75A9.957 9.957 0 0112 5c4.477 0 8.268 2.943 9.542 7a10.05 10.05 0 01-1.402 3.25M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3l18 18" />
           </svg>
         </button>
       </div>
@@ -76,13 +74,13 @@
 
       if(type === 'text') {
         eyeIcon.innerHTML = `
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477 0-8.268-2.943-9.542-7a9.957 9.957 0 012.647-4.435m1.75-1.75A9.957 9.957 0 0112 5c4.477 0 8.268 2.943 9.542 7a10.05 10.05 0 01-1.402 3.25M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3l18 18" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
         `;
       } else {
         eyeIcon.innerHTML = `
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477 0-8.268-2.943-9.542-7a9.957 9.957 0 012.647-4.435m1.75-1.75A9.957 9.957 0 0112 5c4.477 0 8.268 2.943 9.542 7a10.05 10.05 0 01-1.402 3.25M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3l18 18" />
         `;
       }
     });
